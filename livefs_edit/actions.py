mwhudson/livefs-editor

commit 05774719e06a7c882d7e429ecdd5465e18470e7e

interpret $LAYER[n] as a prefix to paths in cp, rm

livefs_edit/actions.py
@@ -156,9 +156,24 @@ def shell(ctxt, command=None):
     run(cmd, cwd=ctxt.p())
 
 
+def interpret_path(ctxt, path):
+    if path.startswith('/'):
+        return path
+    if path.startswith('$LAYERS['):
+        rbracket = path.find(']')
+        if rbracket == -1 or path[rbracket + 1] != '/':
+            raise Exception(f"could not interpret path {path!r}")
+        index = int(path[len('$LAYERS['):rbracket])
+        path = path[rbracket+2:]
+        base = ctxt.edit_squashfs(
+            get_squash_names(ctxt)[index],
+            add_sys_mounts=index == 0)
+        return os.path.join(base, path)
+
+
 @register_action()
 def cp(ctxt, source, dest):
-    shutil.copy(ctxt.p(source, allow_abs=True), ctxt.p(dest, allow_abs=True))
+    shutil.copy(interpret_path(ctxt, source), interpret_path(ctxt, dest))
 
 
 @register_action()
@@ -192,7 +207,7 @@ def rm_f(path):
 
 @register_action()
 def rm(ctxt, path):
-    rm_f(ctxt.p(path, allow_abs=True))
+    rm_f(interpret_path(ctxt, path))
 
 
 def download_snap(ctxt, snap_name, channel):

livefs_edit/context.py
@@ -98,11 +98,10 @@ class EditContext:
     def tmpfile(self):
         return tempfile.mktemp(dir=self.p('.tmp'))
 
-    def p(self, *args, allow_abs=False):
-        if not allow_abs:
-            for a in args:
-                if a.startswith('/'):
-                    raise Exception('no absolute paths here please')
+    def p(self, *args):
+        for a in args:
+            if a.startswith('/'):
+                raise Exception('no absolute paths here please')
         return os.path.join(self.dir, *args)
 
     def add_loop(self, file):

commit 662852d993a96d5977264d3da60bf45fcde79db7

actions: restore support for relative paths
interpret_path() returns None when called for a relative path.

Relative paths should be interpreted as relative to the main temporary
directory.

Fixes: 05774719e06a ("interpret $LAYER[n] as a prefix to paths in cp, rm")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

livefs_edit/actions.py
@@ -169,6 +169,7 @@ def interpret_path(ctxt, path):
             get_squash_names(ctxt)[index],
             add_sys_mounts=index == 0)
         return os.path.join(base, path)
+    return ctxt.p(path)
 
 
 @register_action()