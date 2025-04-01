mwhudson/livefs-editor

commit 4d5ff3104793113602e6d7238b289917dbeb98ba

rewrite get_suite in an excessively fancy way

livefs_edit/context.py
@@ -184,9 +184,11 @@ class EditContext:
             return fp.read().strip().split()[-2]
 
     def get_suite(self):
-        path=glob.glob(self.p('old/iso/dists/*/Release'))
-        suite=path[0].split(os.sep)[6]
-        return suite
+        from deb822 import Deb822
+        paths = glob.glob(self.p('old/iso/dists/*/Release'))
+        with open(paths[0]) as fp:
+            release = Deb822(fp)
+        return release['Suite']
 
     def edit_squashfs(self, name, *, add_sys_mounts=True):
         lower = self.mount_squash(name)

livefs_edit/actions.py
@@ -334,6 +334,9 @@ def add_autoinstall_config(ctxt, autoinstall_config):
 
 @register_action()
 def resign_pool(ctxt, dist=None):
+    if dist is None:
+        dist = ctxt.get_suite()
+
     gpgconf = ctxt.tmpfile()
     gpghome = ctxt.tmpdir()
     with open(gpgconf, 'x') as c:
@@ -360,6 +363,7 @@ Expire-Date: 0
     key_path = f'{new_fs}/etc/apt/trusted.gpg.d/custom-iso-key.gpg'
     with open(key_path, 'w') as new_key:
         run(['gpg', '--home', gpghome, '--export'], stdout=new_key)
+
 
 @register_action()
 def add_debs_to_pool(ctxt, debs: List[str] = ()):

commit b760e70782f98f72f3d85c936341f028b18d8919

context.py: illegal import of deb822
Pylint shows these coding errors:

  livefs_edit/context.py:235:8:
    E0401: Unable to import 'deb822' (import-error)
    C0415: Import outside toplevel (deb822.Deb822)
           (import-outside-toplevel)

'deb822' can be imported from 'debian'.

Fixes: 4d5ff3104793 ("rewrite get_suite in an excessively fancy way")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

livefs_edit/context.py
@@ -23,6 +23,7 @@ import shlex
 import shutil
 import subprocess
 import tempfile
+from debian import deb822
 
 
 class _MountBase:
@@ -232,10 +233,9 @@ class EditContext:
             return fp.read().strip().split()[-2]
 
     def get_suite(self):
-        from deb822 import Deb822
         paths = glob.glob(self.p('old/iso/dists/*/Release'))
         with open(paths[0]) as fp:
-            release = Deb822(fp)
+            release = deb822.Deb822(fp)
         return release['Suite']
 
     def edit_squashfs(self, name, *, add_sys_mounts=True):